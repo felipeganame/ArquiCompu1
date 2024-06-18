a95379817311f589686300bceb8f9fcba825c1cb
felipeganame <101716403+felipeganame@users.noreply.github.com>
2024-06-18 00:29:36 -0300

    Delete copia de seguridad/COPIA SEG 3 directory

--- copia de seguridad/COPIA SEG 3/func_assembly.h
+++
@@ -1,7 +0,0 @@
-#ifndef PROYECTO_ASSEMBLY_FUNCTIONS_H
-#define PROYECTO_ASSEMBLY_FUNCTIONS_H
-
-extern void ambulancia(void);
-extern void secuencia_formula1(void);
-
-#endif //PROYECTO_ASSEMBLY_FUNCTIONS_H
